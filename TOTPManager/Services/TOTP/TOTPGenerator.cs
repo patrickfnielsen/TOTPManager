46f66c2455ee753bdacb81e9e13468c5fa263a65
Patrick Falk Nielsen <patrick@6k.io>
2019-02-08 14:09:43 +0100

    Cleanup TOTP classes

--- TOTPManager/Services/TOTP/TOTPGenerator.cs
+++ TOTPManager/Services/TOTP/TOTPGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using TOTPManager.Models;
 
 namespace TOTP.Services.TOTP
@@ -24,12 +25,60 @@ namespace TOTP.Services.TOTP
 
         public int Generate()
         {
-            return TotpHasher.Hash(_secret, GetCurrentCounter(), Digits, _algorithm);
+            return Hash(_secret, Digits, _algorithm);
+        }
+
+        internal int Hash(byte[] secret, int digits = 6, Algorithm algorithm = Algorithm.SHA1)
+        {
+            var hmac = GetAlgorithm(algorithm, secret);
+            return Hash(hmac, digits);
+        }
+
+        private int Hash(HMAC hmac, int digits = 6)
+        {
+            var iterationNumber = GetCurrentCounter();
+            var counter = BitConverter.GetBytes(iterationNumber);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(counter);
+            }
+
+            var hash = hmac.ComputeHash(counter);
+
+            var offset = hash[hash.Length - 1] & 0xf;
+
+            // Convert the 4 bytes into an integer, ignoring the sign.
+            var binary =
+                ((hash[offset] & 0x7f) << 24)
+                | (hash[offset + 1] << 16)
+                | (hash[offset + 2] << 8)
+                | (hash[offset + 3]);
+
+            var password = binary % (int)Math.Pow(10, digits);
+            return password;
         }
 
         private long GetCurrentCounter()
         {
             return (long)(DateTime.UtcNow - unixEpoch).TotalSeconds / (long)Period;
+        }
+
+        private static HMAC GetAlgorithm(Algorithm algorithm, byte[] secretBytes)
+        {
+            switch (algorithm)
+            {
+                case Algorithm.SHA1:
+                    return new HMACSHA1(secretBytes);
+                case Algorithm.SHA256:
+                    return new HMACSHA256(secretBytes);
+                case Algorithm.SHA512:
+                    return new HMACSHA512(secretBytes);
+                case Algorithm.MD5:
+                    return new HMACMD5(secretBytes);
+                default:
+                    return new HMACSHA1(secretBytes);
+            }
         }
     }
 }
